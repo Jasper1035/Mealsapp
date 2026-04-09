0b89f60e4a6ad9418bb3485dfe25d3f67faf6dd1
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-04-09 22:55:43 +0100

    add the category screen
    set up the project and add the themes , category screen.

---
+++ lib/categories.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project4/categories.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Color.fromARGB(255, 131, 57, 0),
+    brightness: Brightness.dark,
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: theme, home: CategoriesScreen());
+  }
+}
