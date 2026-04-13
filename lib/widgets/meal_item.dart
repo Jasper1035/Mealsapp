6b23ef10dd143b0cd9d5bff34cd8b9ee9df1923b
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-04-13 09:23:38 +0100

    updating the meals screen
    add the imgs and changing the look of the screen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:project4/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key});
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
 
   @override
   Widget build(BuildContext context) {
-    return Text('Meal Item');
+    return Card(
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.black54,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6,
+                  horizontal: 44,
+                ),
+                child: const Column(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
