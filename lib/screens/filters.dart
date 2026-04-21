66f269f247b24e9f9110b56d01814acc9260e326
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-04-21 21:38:04 +0100

    adding a new screen

---
+++ lib/screens/filters.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+  @override
+  State<FiltersScreen> createState() {
+    return _FiltersScreenState();
+  }
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
